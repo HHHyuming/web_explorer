0c0168fff8bf636cdc53102fe04c1dd7c842fc5f
HHHyuming <2219659698@qq.com>
2020-05-16 16:51:14 +0800

    push

---
+++ backend/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    # 基础配置文件load
+
+    # DB load
+
+    # restful load
+
+    # 脚本加载
+
+    # api load
+
+    app.register_blueprint()
+    return app
+
---
+++ backend/app/api/explorer.py
@@ -0,0 +1,13 @@
+import os
+from flask import Blueprint
+explorer = Blueprint('explorer', __name__)
+
+
+@explorer.route('/get_file_list')
+def get_file_list():
+    """
+    获取当前用户家目录下的，--所有文件
+    :return:
+    """
+    
+    return 'get file list'
